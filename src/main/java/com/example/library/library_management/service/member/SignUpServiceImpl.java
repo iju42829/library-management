1e50d6dec552fb358d5d72131593d1fc69a15df7
iju42829 <ijud4282@gmail.com>
2024-08-27 19:38:11 +0900

    fix: SignUpService 트랜잭션 추가

--- src/main/java/com/example/library/library_management/service/member/SignUpServiceImpl.java
+++ src/main/java/com/example/library/library_management/service/member/SignUpServiceImpl.java
@@ -6,9 +6,11 @@ import com.example.library.library_management.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class SignUpServiceImpl implements SignUpService {
 
