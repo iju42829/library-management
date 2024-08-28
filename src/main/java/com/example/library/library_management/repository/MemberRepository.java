936627ddac3c86ea8f415a7205dd2293393dc617
iju42829 <ijud4282@gmail.com>
2024-08-28 15:07:38 +0900

    feat: username으로 사용자 조회 기능 추가

--- src/main/java/com/example/library/library_management/repository/MemberRepository.java
+++ src/main/java/com/example/library/library_management/repository/MemberRepository.java
@@ -3,6 +3,10 @@ package com.example.library.library_management.repository;
 import com.example.library.library_management.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Boolean existsByUsername(String username);
+
+    Optional<Member> findByUsername(String username);
 }
