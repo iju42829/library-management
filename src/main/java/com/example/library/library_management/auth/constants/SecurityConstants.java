1090024e98bb94632fae52bb4a3333ba3b5e5d66
iju42829 <ijud4282@gmail.com>
2024-09-04 19:18:45 +0900

    fix: ROLE 예외 문제 해결

---
+++ src/main/java/com/example/library/library_management/auth/constants/SecurityConstants.java
@@ -0,0 +1,9 @@
+package com.example.library.library_management.auth.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class SecurityConstants {
+    public static final String ROLE_ANONYMOUS = "ROLE_ANONYMOUS";
+}
