6a37b09e73d6c1ea7e0b77ac48d6fe314fca434d
iju42829 <ijud4282@gmail.com>
2024-09-07 13:56:59 +0900

    feat: BookReservation 엔티티 추가

---
+++ src/main/java/com/example/library/library_management/domain/BookReservation.java
@@ -0,0 +1,54 @@
+package com.example.library.library_management.domain;
+
+import com.example.library.library_management.domain.base.BaseTimeEntity;
+import com.example.library.library_management.domain.constants.ReservationStatus;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BookReservation extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    private LocalDate reservationDate;
+    private LocalDate expiryReservationDate;
+
+    private LocalDate loanDate;
+    private LocalDate LoanDeadlineDate;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ReservationStatus reservationStatus;
+
+    private BookReservation(Member member, Book book, LocalDate reservationDate,
+                            LocalDate expiryReservationDate, ReservationStatus reservationStatus) {
+
+        this.member = member;
+        this.book = book;
+        this.reservationDate = reservationDate;
+        this.expiryReservationDate = expiryReservationDate;
+        this.reservationStatus = reservationStatus;
+    }
+
+    public static BookReservation createBookReservation(Member member, Book book, LocalDate reservationDate,
+                                                        LocalDate expiryReservationDate, ReservationStatus reservationStatus) {
+
+        return new BookReservation(member, book, reservationDate, expiryReservationDate, reservationStatus);
+    }
+}
