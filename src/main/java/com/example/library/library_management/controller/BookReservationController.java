f3bf76ee467372952652899f22670d7a3166c1fe
iju42829 <ijud4282@gmail.com>
2024-09-09 14:39:09 +0900

    feat: 도서 예약 승인, 반납 및 조회 기능을 위한 컨트롤러 메서드 추가

--- src/main/java/com/example/library/library_management/controller/BookReservationController.java
+++ src/main/java/com/example/library/library_management/controller/BookReservationController.java
@@ -2,17 +2,22 @@ package com.example.library.library_management.controller;
 
 import com.example.library.library_management.controller.argumentResolver.Login;
 import com.example.library.library_management.dto.book.request.BookReservationCreateRequest;
+import com.example.library.library_management.dto.book.response.BookReservationListResponse;
 import com.example.library.library_management.dto.member.MemberSessionDto;
 import com.example.library.library_management.service.book.BookReservationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -21,6 +26,29 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class BookReservationController {
 
     private final BookReservationService bookReservationService;
+
+    @GetMapping
+    public String retrieveBookReservations(@RequestParam(defaultValue = "1") int page,
+                                           @RequestParam(required = false) String username,
+                                           Model model) {
+
+        PageRequest pageRequest = PageRequest.of(page - 1,
+                10,
+                Sort.by(Sort.Direction.DESC, "createdDate"));
+
+        Slice<BookReservationListResponse> bookReservations = bookReservationService.getBookReservationByUsername(username, pageRequest);
+
+        List<BookReservationListResponse> bookReservationListResponses = bookReservations.getContent();
+
+        model.addAttribute("bookReservationListResponses", bookReservationListResponses);
+        model.addAttribute("searchUsername", username);
+
+        model.addAttribute("currentPage", page);
+        model.addAttribute("hasNextPage", bookReservations.hasNext());
+        model.addAttribute("hasPreviousPage", bookReservations.hasPrevious());
+
+        return "book/list-book-reservations";
+    }
 
     @PostMapping
     public String addBookReservation(@Login MemberSessionDto memberSessionDto,
@@ -39,5 +67,21 @@ public class BookReservationController {
         bookReservationService.reserveBook(memberSessionDto.getUsername(), bookReservationCreateRequest.getBookId());
 
         return "redirect:/";
+    }
+
+    @PatchMapping("/{bookReservationId}/approve")
+    public String approveBookReservation(@PathVariable Long bookReservationId) {
+
+        bookReservationService.approveBookReservation(bookReservationId);
+
+        return "redirect:/book-reservations";
+    }
+
+    @PatchMapping("/{bookReservationId}/return")
+    public String returnBookReservation(@PathVariable Long bookReservationId) {
+
+        bookReservationService.returnBookReservation(bookReservationId);
+
+        return "redirect:/book-reservations";
     }
 }
