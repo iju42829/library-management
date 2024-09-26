2edc6484cec8e9de317acfcb75400a6b18c11a9b
iju42829 <ijud4282@gmail.com>
2024-09-26 22:34:20 +0900

    feat: 댓글 및 멤버 관련 예외 추가

---
+++ src/main/java/com/example/library/library_management/exception/comment/CommentNotFoundException.java
@@ -0,0 +1,4 @@
+package com.example.library.library_management.exception.comment;
+
+public class CommentNotFoundException extends RuntimeException {
+}
---
+++ src/main/java/com/example/library/library_management/exception/member/MemberUnauthorizedException.java
@@ -0,0 +1,4 @@
+package com.example.library.library_management.exception.member;
+
+public class MemberUnauthorizedException extends RuntimeException {
+}
