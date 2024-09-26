fa464c339d0b265bfbb8a98f9068e9affc338d1e
iju42829 <ijud4282@gmail.com>
2024-09-26 20:15:58 +0900

    fix: 엔티티 생성자, 수정자 이름이 저장 안되는 문제 해결

--- src/main/java/com/example/library/library_management/LibraryManagementApplication.java
+++ src/main/java/com/example/library/library_management/LibraryManagementApplication.java
@@ -2,8 +2,13 @@ package com.example.library.library_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
 
 @EnableScheduling
 @EnableJpaAuditing
@@ -14,4 +19,9 @@ public class LibraryManagementApplication {
 		SpringApplication.run(LibraryManagementApplication.class, args);
 	}
 
+	@Bean
+	public AuditorAware<String> auditorProvider() {
+		return () -> Optional
+				.of(SecurityContextHolder.getContext().getAuthentication().getName());
+	}
 }
